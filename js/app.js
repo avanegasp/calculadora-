5781767a8142bf3a0c2751f73f9825c4f2ee87e2
avanegasp <arco527@gmail.com>
2017-09-10 20:49:28 -0500

    Add addition

--- js/app.js
+++ js/app.js
@@ -1,5 +1,33 @@
+$(function(){
 
-$('input').click(function(){
-  var capture = $(this).attr('value')
-  console.log(capture)
+  var v1, v2, op;
+  //Escribir en el input
+  $('input[name=btn]').click(function(){
+    var primeraCifra = $('#resultado').val($('#resultado').val() + $(this).val())
+  })
+
+  // Input volver a cero
+  $('input[name=c]').click(function(){
+    $('#resultado').val('')
+  })
+
+  //Suma
+  $('input[name=suma]').click(function(){
+    if($('#resultado').val() != ''){
+      // v1 = parseFloat($('#resultado').val());
+      $('#resultado').val('')
+      // op = "suma"
+      $('#op').text($(this).val())
+    } else {
+      console.log('coloque una cifra')
+    }
+  });
+
+  $('input[name=igual]').click(function(){
+    if($('#resultado').val() != ''){
+
+    } else {
+      console.log('no hay resultado')
+    }
+  });
 })
