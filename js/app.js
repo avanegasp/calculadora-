fb831d8ac3504f1d3c1cfa6301ee646c4346ce77
avanegasp <arco527@gmail.com>
2017-09-10 22:07:09 -0500

    Calculator done

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ $(function(){
   var v1, v2, op;
   //Escribir en el input
   $('input[name=btn]').click(function(){
-    var primeraCifra = $('#resultado').val($('#resultado').val() + $(this).val())
+    $('#resultado').val($('#resultado').val() + $(this).val())
   })
 
   // Input volver a cero
@@ -14,17 +14,64 @@ $(function(){
   //Suma
   $('input[name=suma]').click(function(){
     if($('#resultado').val() != ''){
-      // v1 = parseFloat($('#resultado').val());
+      v1 = parseFloat($('#resultado').val());
       $('#resultado').val('')
-      // op = "suma"
+      op = "suma"
       $('#op').text($(this).val())
     } else {
-      console.log('coloque una cifra')
+      console.log('sume algo mijo')
     }
   });
 
+  //resta
+  $('input[name=resta]').click(function(){
+    if($('#resultado').val() !=''){
+      v1= parseFloat($('#resultado').val())
+      $('#resultado').val('')
+      op = "resta"
+      $('#op').text($(this).val())
+
+    }else{
+      console.log('reste algo mijo')
+    }
+  })
+
+//multiplicaciones
+$('input[name=multiplicacion]').click(function(){
+  if($('#resultado').val() != ''){
+    v1 = parseFloat($('#resultado').val())
+    $('#resultado').val('')
+    op = "multiplicacion"
+    $('#op').text($(this).val())
+
+  }else{
+    console.log('multiplique algo mijo')
+  }
+})
+
+  $('input[name=division]').click(function(){
+    if($('#resultado').val() != ''){
+      v1= parseFloat($('#resultado').val())
+      $('#resultado').val('')
+      op = "division"
+      $('#op').text($(this).val())
+    }else{
+      console.log('divida algo mijo')
+    }
+  })
+
   $('input[name=igual]').click(function(){
     if($('#resultado').val() != ''){
+      v2 = parseFloat($('#resultado').val());
+
+      if(op == 'suma'){
+        $('#resultado').val(v1 + v2)
+      }if(op == 'resta'){
+        $('#resultado').val(v1 - v2)
+      }if(op == 'multiplicacion'){
+        $('#resultado').val(v1 * v2)
+      }if(op == 'division')
+        $('#resultado').val(v1 / v2)
 
     } else {
       console.log('no hay resultado')
